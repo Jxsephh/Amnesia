1f307a3ed256c6bf123aa16d52b00acf23f28068
Tristan Seifert <tristan@tseifert.me>
2019-03-24 15:06:54 -0500

    serve static resources

--- api/models.py
+++ api/models.py
@@ -4,8 +4,12 @@ import json, ast
 
 #This pretty much comes from https://codeburst.io/this-is-how-easy-it-is-to-create-a-rest-api-8a25122ab1f3
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 api = Api(app)
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 #This opens the JSON file and stores the data into a Python dictionary
 with open("api/nodes.json", 'r') as f:
@@ -13,11 +17,11 @@ with open("api/nodes.json", 'r') as f:
         nodes = ast.literal_eval(json.dumps(jdata))
 
 class Story(Resource):
-    def get(self, id): 
+    def get(self, id):
         for i in range(len(nodes["nodes"])): #For every node, find the matching ID and return the node with this ID
             if(int(id) == nodes["nodes"][i]["id"]):
                 return nodes["nodes"][i], 200
-    
+
         return "Node not found", 404
     def post(self, id):
         parser = reqparse.RequestParser()
@@ -52,7 +56,7 @@ class Story(Resource):
                 node["choiceA"] = args["choiceA"]
                 node["choiceB"] = args["choiceB"]
                 return node, 200
-        
+
         node = {
             "id": id,
             "dialogue": args["dialogue"],
